d9942b12ec657842e91f7317a00e4acc7e718bf8
Theophilus K. Dadzie <tkdadzie@st.ug.edu.gh>
2023-05-01 12:36:16 -0400

    added

--- 01-starting-setup8/src/App.js
+++ 01-starting-setup8/src/App.js
@@ -1,14 +1,24 @@
 import React, { useState } from 'react';
 
 import './App.css';
+import Button from './components/UI/Button/Button';
+import DemoOutput from './components/Demo/DemoOuput';
 
 function App() {
 
-  useState();
+  const [showParagraph, setShowParagraph] = useState(false);
+ 
+  console.log('APP RUNNING'); 
+  const toggleParagraphHandler = () => {
+    // setShowParagraph(!showParagraph);
+    setShowParagraph(prevshowParagraph => !prevshowParagraph);
+  };
 
   return (
     <div className="app">
       <h1>Hi there!</h1>
+      <DemoOutput show={showParagraph}/>
+      <Button onClick={toggleParagraphHandler}> Toggle Paragraph! </Button>
     </div>
   );
 }
