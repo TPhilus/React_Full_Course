da63694275f14ce38675f3c48831be7641c797ce
Theophilus K. Dadzie <tkdadzie@st.ug.edu.gh>
2023-05-01 11:01:25 -0400

    project 8

---
+++ 01-starting-setup8/src/App.js
@@ -0,0 +1,16 @@
+import React, { useState } from 'react';
+
+import './App.css';
+
+function App() {
+
+  useState();
+
+  return (
+    <div className="app">
+      <h1>Hi there!</h1>
+    </div>
+  );
+}
+
+export default App;
