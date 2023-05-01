278cd2937efd4ddfee5f2ad9c656762165e9e9c2
Theophilus K. Dadzie <tkdadzie@st.ug.edu.gh>
2023-04-30 20:17:05 -0400

    added Modal

--- food-app/src/Components/Cart/Cart.jsx
+++ food-app/src/Components/Cart/Cart.jsx
@@ -1,18 +1,19 @@
 import React from "react";
 
 import classes from "./Cart.module.css";
+import Modal from "../UI/Modal";
 
 const Cart = () => {
   const cartItem = (
     <ul className={classes['cart-items']}>
-      {[{ id: "c1", name: "Theo", amount: 2, price: 12.99 }].map((item) => (
+      {[{ id: "c1", name: "@Philus", amount: 2, price: 12.99 }].map((item) => (
         <li>{item.name}</li>
       ))}
     </ul>
   );
 
   return (
-    <div>
+    <Modal>
       {cartItem}
       <div className={classes.total}>
         <span>Total Amount</span>
@@ -22,7 +23,7 @@ const Cart = () => {
         <button className={classes['button--alt']}>Close</button>
         <button className={classes.button}>Order</button>
       </div>
-    </div>
+    </Modal>
   );
 };
 
