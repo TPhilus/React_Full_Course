3be3305649efad2e495de9c231fb468d254da5a3
Theophilus K. Dadzie <tkdadzie@st.ug.edu.gh>
2023-04-30 21:23:05 -0400

    end of manageing cart and modal state

--- food-app/src/Components/Cart/Cart.jsx
+++ food-app/src/Components/Cart/Cart.jsx
@@ -3,24 +3,24 @@ import React from "react";
 import classes from "./Cart.module.css";
 import Modal from "../UI/Modal";
 
-const Cart = () => {
+const Cart = (props) => {
   const cartItem = (
     <ul className={classes['cart-items']}>
       {[{ id: "c1", name: "@Philus", amount: 2, price: 12.99 }].map((item) => (
-        <li>{item.name}</li>
+        <li key={item.id}>{item.name}</li>
       ))}
     </ul>
   );
 
   return (
-    <Modal>
+    <Modal onClose={props.onClose}>
       {cartItem}
       <div className={classes.total}>
         <span>Total Amount</span>
         <span>35.62</span>
       </div>
       <div className={classes.actions}>
-        <button className={classes['button--alt']}>Close</button>
+        <button className={classes['button--alt']} onClick={props.onClose} >Close</button>
         <button className={classes.button}>Order</button>
       </div>
     </Modal>
--- food-app/src/Components/Layout/Header.jsx
+++ food-app/src/Components/Layout/Header.jsx
@@ -8,13 +8,13 @@ const Header = (props) => {
         <Fragment>
             <header className={classes.header}>
                 <h1>ReactMeals</h1>
-                <HeaderCartButton />
+                <HeaderCartButton onClick={props.onShowCart} />
             </header>
             <div className={classes['main-image']}>
                 <img src={mealsImage} alt="A table full of delicious food!" />
             </div>
         </Fragment>
     );
-};
+}; 
 
 export default Header;
